ffb24419a7bf6ea4bf405bafa9c3182316039e7c
Marcelo Santos <marcelo.santos.dev@gmail.com>
2020-08-18 13:25:55 -0300

    Add Buttons

--- App.js
+++ App.js
@@ -1,114 +1,83 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import { SafeAreaView} from 'react-native';
+import Lottie from 'lottie-react-native'
+import { StyleSheet, View, Button, Text, TouchableOpacity } from 'react-native'
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import pizza from './pizza.json'
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+const TestLottie = () => {
+  return <SafeAreaView style={[styles.container]}>
+
+    <Lottie style={[styles.lottie]} resizeMode="contain" autoSize source={pizza} autoPlay loop />
+
+    <View style={[styles.buttons]}>
+
+        <TouchableOpacity style={[styles.btnLogin]}>
+          <Text style={[styles.loginText]}>Login</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={[styles.btnSingUp]}>
+          <Text style={[styles.singupText]}>Sing Up</Text>
+        </TouchableOpacity>
+
+    </View>
+
+  </SafeAreaView>
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  container: {
+    flex: 1, 
+    justifyContent: 'space-evenly', 
+    alignItems: 'center', 
+    flexDirection: 'column',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  title: {
+    fontSize: 50, 
+    fontWeight: 'bold',
+    color: 'orange',
   },
-  body: {
-    backgroundColor: Colors.white,
+  lottie: {
+    width: 250, 
+    height: 250, 
   },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  buttons: {
+    padding: 0,
+    width: 250,
+    height: 200,
+    flexDirection: 'column', 
+    justifyContent: 'space-evenly',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
+  btnLogin: { 
+    width: "100%",
+    height: 60,
+    backgroundColor: "orange",
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
+  loginText: {
+    color: '#FFF',
+    fontSize: 25,
+    fontWeight: 'bold'
   },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
+  btnSingUp: {
+    width: "100%",
+    height: 60,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    borderColor: 'orange',
+    borderWidth: 4,
 
-export default App;
+  },
+  singupText: {
+    color: 'orange',
+    fontSize: 25,
+    fontWeight: 'bold'
+  }
+})
+
+export default TestLottie;
